835f36d6b19bfcd4aa2b8932da737bee236f4661
pangil5634 <oksk6681@naver.com>
2023-05-18 15:27:31 +0900

    JC is very handsome.

--- app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -12,8 +12,8 @@ public class UsingBufferedReader {
         //-------------- Test reading 1 MB file. --------------------
 
         StopWatch.start();
-
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        String filename1 = args[0];
+        BufferedReader inputStream= new BufferedReader(new FileReader(filename1));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -25,8 +25,8 @@ public class UsingBufferedReader {
         //-------------- Test reading 10 MB file. --------------------
 
         StopWatch.start();
-
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        String filename2 = args[1];
+        BufferedReader inputStream2= new BufferedReader(new FileReader(filename2));
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
